222bd9255f3daf8fa7b636e41b5337f62202e9ee
Raul Alejandro Giron jimenez <raulgiron@hotmail.com>
2021-08-17 02:34:41 -0400

    This is just a test of creating files in my computer and uploading to GitHub

--- second_learning_file.py
+++ second_learning_file.py
@@ -1,1 +1,1 @@
-print("This new file is just to learn to create files in my repos here in GitHub.")
+print("This new file is just to learn to create files in my repos here in GitHub, and this was mande in my computer prior to upload it to GitHub")
---
+++ third_learning_file.py
@@ -0,0 +1,6 @@
+def home_made():
+	print('This was entirely made in my computer prior to upload it to GitHub')
+
+
+if __name__ == "__main__":
+	home_made()
