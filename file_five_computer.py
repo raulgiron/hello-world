27498d0a2743063af94e3c1dc517610ed873466a
Raul Alejandro Giron jimenez <raulgiron@hotmail.com>
2021-08-17 19:14:49 -0400

    Learing a lot of things

---
+++ file_five_computer.py
@@ -0,0 +1,9 @@
+# This file has no importance. And it was create in my computer.
+
+def sumatoria(n: int = 0, m: int = 0):
+    total = n + m
+    return total
+
+
+print(sumatoria(4, 4))
+print(sumatoria())
